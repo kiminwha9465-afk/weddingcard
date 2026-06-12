06cb9573517c479eaabf4ba129ab3c670c10ca89
kiminwha9465-afk <kiminwha9465@gmail.com>
2026-06-12 12:00:58 +0900

    Add files via upload

---
+++ weddingcard/src/test/java/org/example/weddingcard/WeddingcardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.weddingcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeddingcardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
